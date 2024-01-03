94d5eded6facc71f2e86a7a59d639049b226e12f
Adrian Stefan Moroianu <moroianu.stefan33@gmail.com>
2024-01-03 17:50:23 +0200

    add character_sprite_sheets

--- main.py
+++ main.py
@@ -17,6 +17,41 @@ PLAYER_VEL = 5
 
 winodws = pygame.display.set_mode((WIDTH, HEIGHT))
 
+#functia flip imi intoarce imaginea pe axa x (adica ii da flip)
+def flip(sprites):
+    return [pygame.transform.flip(sprite, True, False) for sprite in sprites]
+
+def load_sprites_sheets(dir1, dir2, width, height, direction = False):
+    path = join("assets", dir1, dir2)
+    images = [f for f in listdir(path) if isfile(join(path, f))]
+
+    all_sprites = {}
+
+    for image in images:
+        sprite_sheet = pygame.image.load(join(path, image)).convert_alpha()
+        #convert_alpha() este o functie care imi face imaginea transparenta (png)
+
+        sprites = []
+        for i in range(sprite_sheet.get_width() // width):
+            surface = pygame.Surface((width, height), pygame.SRCALPHA, 32)
+            # SRCALPHA este un flag care imi face imaginea transparenta
+            rect = pygame.Rect(i * width, 0, width, height)
+            surface.blit(sprite_sheet, (0, 0), rect)
+            #blit este o functie care imi copiaza imaginea
+            sprites.append(pygame.transform.scale2x(surface))
+
+        if direction:
+            all_sprites[image.replace(".png", "") + "_right"] = sprites
+            all_sprites[image.replace(".png", "") + "_left"] = flip(sprites)
+        else:
+            all_sprites[image.replace(".png", "")] = sprites
+
+    return all_sprites
+
+
+
+
+
 
 
 
@@ -24,8 +59,11 @@ class Player(pygame.sprite.Sprite):
     #clasa Player mosteneste clasa Sprite pentru a face o coliziune perfecta intre Sprite-uri
     COLOR = (255, 0, 0)
     GRAVITY = 1
+    SPRITES = load_sprites_sheets("MainCharacters", "Wizard", 32, 32, True)
+
     def __init__(self, x, y, width, height):
         self.rect = pygame.Rect(x, y, width, height)
+        #rect este un box collider - adica un dreptunghi care imi face coliziunea perfecta
         self.x_vel = 0
         self.y_vel = 0
         self.mask = None
@@ -50,13 +88,14 @@ class Player(pygame.sprite.Sprite):
             self.animation_count = 0
 
     def loop(self, fps):
-        self.y_vel += min(1, (self.fall_count / fps) * self.GRAVITY)
+        #self.y_vel += min(1, (self.fall_count / fps) * self.GRAVITY)
         self.move(self.x_vel, self.y_vel)
 
         self.fall_count += 1
 
     def draw(self, window):
-        pygame.draw.rect(window, self.COLOR, self.rect)
+        self.sprite = self.SPRITES["idle_" + self.direction][0]
+        window.blit(self.sprite, (self.rect.x, self.rect.y))
 
 
 
